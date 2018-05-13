eadfaa24322f76b328a3a0b090e245cd3cbde6d4
MarselVaida <marsel.vaida@gmail.com>
2018-05-13 20:14:36 +0300

    add new print

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main (String [] args){
 // print hello world on screen 
         System.out.println("hello world");
+		System.out.println("marsel");
     }
 }
